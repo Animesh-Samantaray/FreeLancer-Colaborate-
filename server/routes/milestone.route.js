456cdf87618bdd15cef0ab2fab7b96602b864b5e
Animesh Samantaray <animeshsamantaray2@gmail.com>
2026-08-06 13:54:42 +0530

    completed milestone branch

---
+++ server/routes/milestone.route.js
@@ -0,0 +1,31 @@
+import express from "express";
+import {
+  createMilestone,
+  getProjectMilestones,
+  getMilestoneById,
+  updateMilestone,
+  deleteMilestone,
+  updateMilestoneStatus,
+} from "../controllers/milestone.controller.js";
+import { verifyToken } from "../middleware/auth.middleware.js";
+const router = express.Router();
+
+// Create milestone (Client/Admin)
+router.post("/", verifyToken, createMilestone);
+
+// Get all milestones of a project
+router.get("/project/:projectId", verifyToken, getProjectMilestones);
+
+// Get single milestone
+router.get("/:id", verifyToken, getMilestoneById);
+
+// Update milestone details
+router.put("/:id", verifyToken, updateMilestone);
+
+// Update only milestone status
+router.patch("/:id/status", verifyToken, updateMilestoneStatus);
+
+// Delete milestone
+router.delete("/:id", verifyToken, deleteMilestone);
+
+export default router;
